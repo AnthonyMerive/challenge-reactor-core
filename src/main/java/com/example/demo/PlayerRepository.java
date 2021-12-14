6106ce6eed833b422404a00a1fdba3cb3a61ce11
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-12-14 13:36:29 -0500

    limpiando un poco el codigo

--- src/main/java/com/example/demo/PlayerRepository.java
+++ src/main/java/com/example/demo/PlayerRepository.java
@@ -1,10 +1,8 @@
 package com.example.demo;
 
-import com.example.demo.Player;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.*;
 
 //MongoDB Reactive Repository
 
